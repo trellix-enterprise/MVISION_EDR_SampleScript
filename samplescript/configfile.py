9552fb7027ded8a10682b63566f0bd2eb9242199
spriyada <sophia.priyadarshini@trellix.com>
2023-05-25 11:59:12 -0700

    Adding clientid and secret

--- samplescript/configfile.py
+++ samplescript/configfile.py
@@ -4,16 +4,17 @@ iam_url='https://preprod.iam.mcafee-cloud.com/iam/v1.0/token'
 
 #Headers to be included
 x_api_key="ECYPmTx9g01rWmT3TXUTs8mNkHjbaNSv7lAp6uov"
-client_id="0oae6vfj6gCEIsMId0h7"
-grant_type = "password"
+#client_id="0oae6vfj6gCEIsMId0h7"
+#audience: 'mcafee'
+grant_type = "client_credentials"
 scope = "epo.admin mi.user.config mi.user.investigate gsd.a.e soc.rts.c soc.rts.r soc.hts.c soc.hts.r soc.act.tg"
 
 # User to update tenant credentials
-client_userid="test2122234@yopmail.com" #"test3132334@yopmail.com" #"test2122234@yopmail.com"
-client_credentials="Trellix@1234" 
+client_userid="Z3C5ke6wQeVFnC-0LLSDmgHM6" #"test3132334@yopmail.com" #"test2122234@yopmail.com"
+client_credentials="wF-EJpHq73FQm1mmhNzC399D1" 
 # For get-threat API please use below tenant as threats are present here
-gtclient_id="vasav-lowerint-3@yopmail.com"
-gtclient_credentials="Mcafee@123" 
+gtclient_id="Z3C5ke6wQeVFnC-0LLSDmgHM6"
+gtclient_credentials="wF-EJpHq73FQm1mmhNzC399D1" 
 # User to update the host machine name for hostbased remediation
 host_name = "DESKTOP-BUV2HRF"#DESKTOP-2HS9OV4"
 
